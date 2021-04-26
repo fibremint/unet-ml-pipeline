5f5d20c381ebb5da3de40be8dcc4ccef4de8d294
Hyunjong Kim <fibremint@gmail.com>
2021-04-26 23:15:41 +0900

    Code refactoring

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -2,11 +2,11 @@ import os
 
 from kfp import dsl, compiler
 
-import pipeline_ops as po
+from . import ops
 
 
-@dsl.pipeline(name='ct-pipeline')
-def nwd_pipeline():
+@dsl.pipeline(name='continuous-training-pipeline')
+def continuous_training_pipeline():
     batch_size = 64
     gpu_num = 1
     seed = 42
@@ -15,16 +15,16 @@ def nwd_pipeline():
 
     # tf_memory_limit = '7G'
 
-    check_preprocess_proceed_task = po.check_preprocess_proceed_op()
+    check_preprocess_proceed_task = ops.check_preprocess_proceed_op()
     is_preprocess_required = check_preprocess_proceed_task.outputs['is_preprocess_required']
 
     with dsl.Condition(is_preprocess_required == 'True', name='preprocess-required'):
         preprocess_required_slide_paths = check_preprocess_proceed_task.outputs['preprocess_required_slide_paths']
 
-        data_preprocess_task = po.data_preprocess_op(preprocess_required_slide_paths)
+        data_preprocess_task = ops.data_preprocess_op(preprocess_required_slide_paths)
         data_preprocess_task.after(check_preprocess_proceed_task)
 
-        train_task = po.train_op(batch_size=batch_size,
+        train_task = ops.train_op(batch_size=batch_size,
                                  seed=seed)
 
         train_task.set_gpu_limit(gpu_num)
@@ -34,7 +34,7 @@ def nwd_pipeline():
         train_task.after(data_preprocess_task)
         target_checkpoints_path = train_task.outputs['target_checkpoints_path']
 
-        eval_task = po.evaluate_op(target_checkpoints_path=target_checkpoints_path,
+        eval_task = ops.evaluate_op(target_checkpoints_path=target_checkpoints_path,
                                    prediction_threshold=eval_prediction_threshold,
                                    seed=seed)
         eval_task.set_gpu_limit(gpu_num)
@@ -43,7 +43,7 @@ def nwd_pipeline():
         target_checkpoints_path = eval_task.outputs['target_checkpoints_path']
         checkpoint_metrics_filename = eval_task.outputs['checkpoint_metrics_filename']
 
-        check_deployable_task = po.check_deployable_op(target_checkpoints_path=target_checkpoints_path,
+        check_deployable_task = ops.check_deployable_op(target_checkpoints_path=target_checkpoints_path,
                                                        checkpoint_metrics_filename=checkpoint_metrics_filename)
 
         check_deployable_task.after(eval_task)
@@ -52,30 +52,34 @@ def nwd_pipeline():
         is_deployable_checkpoint_exist = check_deployable_task.outputs['is_deployable_checkpoint_exist']
 
         with dsl.Condition(is_deployable_checkpoint_exist == 'True', name='exist'):
-            update_deploy_config_task = po.update_deploy_config_op(
+            update_deploy_config_task = ops.update_deploy_config_op(
                 deployable_checkpoint_info=deployable_checkpoint_info)
 
             update_deploy_config_task.after(check_deployable_task)
 
-            deploy_task = po.deploy_op()
+            deploy_task = ops.deploy_op()
             deploy_task.after(update_deploy_config_task)
 
         with dsl.Condition(is_deployable_checkpoint_exist == 'False', name='not-exist'):
-            po.print_op('deployed checkpoint: previous')
+            ops.print_op('deployed checkpoint: previous')
 
     with dsl.Condition(is_preprocess_required == 'False', name='preprocess-not-required'):
-        print_task = po.print_op('all of the slides are pre-processed')
+        print_task = ops.print_op('all of the slides are pre-processed')
 
 
-if __name__ == '__main__':
+def create_continuous_training_workflow():
     #ref: https://towardsdatascience.com/build-your-data-pipeline-on-kubernetes-using-kubeflow-pipelines-sdk-and-argo-eef69a80237c
-    workflow_dict = compiler.Compiler()._create_workflow(nwd_pipeline)
+    workflow_dict = compiler.Compiler()._create_workflow(continuous_training_pipeline)
     # ref: https://github.com/argoproj/argo-workflows/issues/4534
-    # Failed to establish pod watch: timed out waiting for the condition
+    # resolves for: Failed to establish pod watch: timed out waiting for the condition
     workflow_dict['metadata']['namespace'] = 'argo'
     workflow_dict['spec']['serviceAccountName'] = 'default'
 
-    if not os.path.exists('./output'):
-        os.makedirs('./output')
+    workflow_dict['kind'] = 'WorkflowTemplate'
+    del workflow_dict['metadata']['generateName']
+    workflow_dict['metadata']['name'] = 'continuous-training-template'
 
-    compiler.Compiler._write_workflow(workflow_dict, './output/pipeline.yml')
+    if not os.path.exists('../output'):
+        os.makedirs('../output')
+
+    compiler.Compiler._write_workflow(workflow_dict, '../output/continuous-training-workflow-template.yml')
